df25f04c40db3ead56101684383ef989e43dffe5
yaseen177 <yaseen.hussain18@outlook.com>
2026-01-08 15:46:32 +0000

    fix css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // This imports App.tsx
-import './index.css';
+import App from './App';
+import './index.css'; // <--- THIS LINE IS LIKELY MISSING
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
