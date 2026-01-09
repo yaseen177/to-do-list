6530f7a3612391a69fa13f4e2be5182dbeab4241
yaseen177 <yaseen.hussain18@outlook.com>
2026-01-09 17:39:12 +0000

    dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}"  // <--- The magic fix is adding 'ts' and 'tsx' here
   ],
+  DARKmODE: 'class',
   theme: {
     extend: {
       fontFamily: {
